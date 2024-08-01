28033c352b4e19c618d2d934125c294dc681c8f0
Basanta-khatri-0311 <khatribasanta.git@gmail.com>
2024-08-01 23:26:24 +0545

    var variable can be used anywhere whereas let variabke can only be used inside a block

--- Javascript/002_variables_js/script.js
+++ Javascript/002_variables_js/script.js
@@ -1,12 +1,16 @@
-let firstName = 'Basanta'
-let lastName = 'Khatri'
+{
+  var firstName = 'Basanta'
+}
+{
+  let lastName = 'Khatri'
+}
 let age = 25
 firstName =100
 
 let isHappy = true
 let a
 a='Basanta'
-let userIntro = 'Hi, my name is  ' + a + ' ' + lastName +'.' +' I am ' + age + ' years old.'
+let userIntro = 'Hi, my name is  ' + firstName + ' ' + lastName +'.' +' I am ' + age + ' years old.'
 
 const hours=24
 

bb264d2f0f3013c44523f077568220a16497f540
Basanta-khatri-0311 <khatribasanta.git@gmail.com>
2024-08-01 23:20:31 +0545

    const cannot be changed

--- Javascript/002_variables_js/script.js
+++ Javascript/002_variables_js/script.js
@@ -1,5 +1,14 @@
-let userName = 'Basanta'
-let userIntro = 'My name is Basanta Khatri.'
+let firstName = 'Basanta'
+let lastName = 'Khatri'
 let age = 25
+firstName =100
 
-let isHappy =true
+let isHappy = true
+let a
+a='Basanta'
+let userIntro = 'Hi, my name is  ' + a + ' ' + lastName +'.' +' I am ' + age + ' years old.'
+
+const hours=24
+
+hours=hours+2
+

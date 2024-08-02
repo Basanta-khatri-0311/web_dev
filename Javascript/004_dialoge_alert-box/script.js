4a46c4ed4cefc70671d7a5f37fbb6c1acb072a15
Basanta-khatri-0311 <khatribasanta.git@gmail.com>
2024-08-02 11:27:19 +0545

    3 types of dialogue box/alert box with different return types and use

---
+++ Javascript/004_dialoge_alert-box/script.js
@@ -0,0 +1,4 @@
+// const alertResult = alert("your message after reloaded.")
+// const isConfirmed = confirm("You want to confirm?")
+const promptResult = prompt("Please provide your name to further continue.")
+console.log(promptResult);

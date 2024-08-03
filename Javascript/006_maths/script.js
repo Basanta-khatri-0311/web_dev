810d8f225ae15c04df7fa61423fe371e95382165
Basanta-khatri-0311 <khatribasanta.git@gmail.com>
2024-08-03 21:52:39 +0545

    sing math object for doing the mathametical calculations

---
+++ Javascript/006_maths/script.js
@@ -0,0 +1,11 @@
+
+
+console.log('The calculation of  15 % 4 which gives the reminder ',15%4);
+console.log(`The calculation of 3 power 3 is `,3**3);
+
+console.log(Math.PI); 
+
+console.log(Math.sqrt(25));
+console.log(Math.sqrt(27));
+
+console.log(Math.pow(2,6));

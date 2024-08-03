609916fb6c5589212099e4c0f4ff0ed095aa9087
Basanta-khatri-0311 <khatribasanta.git@gmail.com>
2024-08-03 22:18:45 +0545

    round off and generating random numbers in js also calculating area of a rectangle.

--- Javascript/006_maths/script.js
+++ Javascript/006_maths/script.js
@@ -1,14 +1,14 @@
 
 
-console.log('The calculation of  15 % 4 which gives the reminder ',15%4);
-console.log(`The calculation of 3 power 3 is `,3**3);
+console.log('The calculation of  15 % 4 which gives the reminder ', 15 % 4);
+console.log(`The calculation of 3 power 3 is `, 3 ** 3);
 
-console.log(Math.PI); 
+console.log(Math.PI);
 
 console.log(Math.sqrt(25));
 console.log(Math.sqrt(27));
 
-console.log(Math.pow(2,6));
+console.log(Math.pow(2, 6));
 
 console.log(Math.floor(2.5));
 console.log(Math.floor(2.99889));
@@ -16,3 +16,17 @@ console.log(Math.floor(-2.5));
 
 console.log(Math.ceil(-2.5));
 console.log(Math.ceil(3.5));
+
+console.log(Math.round(3.5));
+
+console.log(Math.round(Math.random() * 20));
+console.log(Math.round(Math.random() * 20));
+
+//calculating the area of rectangle in js.
+
+const breadth = 30
+const length = 20
+
+let area
+
+console.log(`The area of rectangle of length ${length} and breadth ${breadth} is ${length * breadth}`);

eb6ee3e409c1c46d2e8dc4f90069217fe733bc8f
Basanta-khatri-0311 <khatribasanta.git@gmail.com>
2024-08-05 23:12:45 +0545

    condition in js

---
+++ Javascript/010_decisionmaking/script.js
@@ -0,0 +1,9 @@
+const userName = `Basanta`
+const userAge = 28
+
+console.log(`Name: ${userName}`);
+console.log(`Age: ${userAge}`);
+
+if (false) 
+  console.log(`User is a Working Professional.`)
+

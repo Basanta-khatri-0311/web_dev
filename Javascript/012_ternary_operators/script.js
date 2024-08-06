de0a98caa275bc2f83f8cd95e32fa3476b35b2a8
Basanta-khatri-0311 <khatribasanta.git@gmail.com>
2024-08-06 14:57:36 +0545

    attaching he /she according to the gender using ternary operator

---
+++ Javascript/012_ternary_operators/script.js
@@ -0,0 +1,5 @@
+// const userName = 5 > 2 ? `Basanta khatri` : `Unknown`
+
+// const gender = 'F'
+// const userMessage = `${gender.toLocaleLowerCase() === 'f' ? 'She' : 'He'} is a college Student.`
+// console.log(userMessage)

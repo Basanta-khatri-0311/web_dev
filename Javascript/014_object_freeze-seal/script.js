1d956959bef545481143df4a77a3e6f807a921d6
Basanta-khatri-0311 <khatribasanta.git@gmail.com>
2024-08-07 18:33:11 +0545

    object freezing and seal

---
+++ Javascript/014_object_freeze-seal/script.js
@@ -0,0 +1,15 @@
+const user = {
+  firstName : 'Basanta' ,
+  lastName: 'Khatri' ,
+  add: {
+    city : 'Butwal',
+    postCode: '001122',
+    state : 'Lumbini',
+    details :{
+      population : 12345678,
+      area : '1234 sq km'
+    },
+  },
+  age:15 ,
+  isGraduate:false ,
+}

ffd7a3a94b9febcdc2bde3f1b171307b7f0347dc
Basanta-khatri-0311 <khatribasanta.git@gmail.com>
2024-08-07 14:57:37 +0545

    creating and accessing object using . and bracket method and also nesting object inside object

---
+++ Javascript/001_datatypes/script.js
@@ -0,0 +1,40 @@
+
+// const user = {
+//   firstName: 'Basanta',
+//   lastName: 'Khatri',
+//   age: 22
+// }
+
+// const user2 = {
+//   firstName: 'Basanta',
+//   lastName: 'Khatri',
+//   age: 22
+// }
+
+
+
+const myName = 'Basanta'
+
+const userName1 =''
+const userName2 =''
+
+
+const user1 = {
+  firstName: 'Myname'
+}
+
+const user2 = {
+  firstName: 'Mysecondname',
+  'last-Name': 'Khatri',
+  address: {
+    city: 'butwal',
+    province: 7
+  }
+}
+
+user2.age = 22
+// user2["student-grad"] = true
+
+// console.log(user2.firstName+' ' +user2["last-Name"]);
+
+

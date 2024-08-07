c950b19ad267541eb43b9df71fe2a43956c18348
Basanta-khatri-0311 <khatribasanta.git@gmail.com>
2024-08-08 00:45:11 +0545

    adding the value in such a way that there are only 4 values and we are putting the value at 10th index will put the value in the 10th place but the middle parts will create empty slots with undefined type

---
+++ Javascript/015_arrays/script.js
@@ -0,0 +1,4 @@
+// const fruitsCollection = ['Apple', 'Banana', 'Grapes', 23, true, null, undefined, { name: 'Basanta', age: 21, kke: { add: 'Nepal' } }]
+const fruitsCollection = ['Apple', 'Banana', 'Grapes', 23, true, null, undefined, { name: 'Basanta'}]
+
+fruitsCollection[10] = 'Papaya'

b2166554e782e537e4d2f2b24198cc0edfff842a
Basanta-khatri-0311 <khatribasanta.git@gmail.com>
2024-08-08 01:12:20 +0545

    adding key values in array since it is an object

--- Javascript/015_arrays/script.js
+++ Javascript/015_arrays/script.js
@@ -1,4 +1,16 @@
 // const fruitsCollection = ['Apple', 'Banana', 'Grapes', 23, true, null, undefined, { name: 'Basanta', age: 21, kke: { add: 'Nepal' } }]
 const fruitsCollection = ['Apple', 'Banana', 'Grapes', 23, true, null, undefined, { name: 'Basanta'}]
 
-fruitsCollection[10] = 'Papaya'
+const rainbowColors = ['red','orange','yellow']
+
+// rainbowColors.push('violent') //adding the value in an array using method push
+rainbowColors.push('green','blue','indigo','violent') //adding multiple values in an array using method push
+
+// rainbowColors.pop()//removes an element from last
+
+
+// const newArray =[]
+// newArray.name = 'Basanta'
+
+
+

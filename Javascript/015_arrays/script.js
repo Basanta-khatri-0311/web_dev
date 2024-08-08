fb8d13f9435bf4646e7672a9e29e281469fc38c3
Basanta-khatri-0311 <khatribasanta.git@gmail.com>
2024-08-08 14:27:44 +0545

    array and their methods

--- Javascript/015_arrays/script.js
+++ Javascript/015_arrays/script.js
@@ -1,16 +1,38 @@
-// const fruitsCollection = ['Apple', 'Banana', 'Grapes', 23, true, null, undefined, { name: 'Basanta', age: 21, kke: { add: 'Nepal' } }]
-const fruitsCollection = ['Apple', 'Banana', 'Grapes', 23, true, null, undefined, { name: 'Basanta'}]
+// // const fruitsCollection = ['Apple', 'Banana', 'Grapes', 23, true, null, undefined, { name: 'Basanta', age: 21, kke: { add: 'Nepal' } }]
+// const fruitsCollection = ['Apple', 'Banana', 'Grapes', 23, true, null, undefined, { name: 'Basanta'}]
 
-const rainbowColors = ['red','orange','yellow']
+// const rainbowColors = ['red','orange','yellow']
 
-// rainbowColors.push('violent') //adding the value in an array using method push
-rainbowColors.push('green','blue','indigo','violent') //adding multiple values in an array using method push
+// // rainbowColors.push('violent') //adding the value in an array using method push
+// rainbowColors.push('green','blue','indigo','violent') //adding multiple values in an array using method push
 
-// rainbowColors.pop()//removes an element from last
+// // rainbowColors.pop()//removes an element from last
 
 
-// const newArray =[]
-// newArray.name = 'Basanta'
+// // const newArray =[]
+// // newArray.name = 'Basanta'
+
+
+
+const evenNumbers = [0,2,4,6,8]
+const oddNumbers = [1,3,5,7,9]
+
+const animals = ['dog','cat','rat','lion']
+
+// evenNumbers.shift()
+// evenNumbers.unshift()
+
+// animals.concat(evenNumbers)
+// animals.concat(evenNumbers,oddNumbers)
+
+// evenNumbers.indexOf(2)
+// oddNumbers.includes(3)
+// animals.reverse()
+
+// animals.sort()
+
+// animals.slice()
+
 
 
 

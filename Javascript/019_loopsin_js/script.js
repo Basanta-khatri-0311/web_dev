39274cfec27da27e0dd34fbad76c6e788407f531
Basanta-khatri-0311 <khatribasanta.git@gmail.com>
2024-08-09 14:41:26 +0545

    while loop in js

---
+++ Javascript/019_loopsin_js/script.js
@@ -0,0 +1,28 @@
+console.log('Program Started');
+
+// let num = 0
+
+// while(num < 10){
+//   console.log(num)
+//   num ++
+// }
+// console.log('Program Ended');
+
+// let i = 1
+
+// while(i <= 100){
+//   console.log(i)
+//   i++
+// }
+
+let friends = ['Ram', 'Shyam', 'Hari', 'Sita', 'Gita']
+
+i = 0
+
+while (i < friends.length) {
+  console.log(`${i + 1}. ${friends[i]} `);
+  friends[i] = friends[i] + ' coder'
+  i++
+}
+
+console.log('Program Ended');

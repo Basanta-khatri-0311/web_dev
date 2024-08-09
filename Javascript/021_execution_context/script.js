306a0717e89f83e15ca32d4e473ff1a8f7bba856
Basanta-khatri-0311 <khatribasanta.git@gmail.com>
2024-08-10 01:21:44 +0545

    recursion, overflow study

---
+++ Javascript/021_execution_context/script.js
@@ -0,0 +1,42 @@
+// debugger
+// console.log(firstName);
+
+// var firstName = 'Basanta'
+// let lastName = 'Khatri'
+// let age = 22
+// const yearOfBirth = 2002
+
+
+
+// console.log(userName);
+
+
+// sayHi()
+
+
+// const userName = 'Basanta'
+// const age = 22
+
+// function sayHi() {
+//   const a = 12
+//   const b = 14
+//   add(7,8)
+// }
+
+// function add(x,y){
+//   kei()
+//   return x+y
+// }
+
+
+// function kei(){
+//   console.log('kei kei');
+// }
+
+
+function introduceMe(){
+  console.log('Hi, my name is Basanta');
+  // introduceMe()
+}
+
+introduceMe()

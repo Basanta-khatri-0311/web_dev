ed9901d8cac9434d696766cd2cec27e7bc01dd83
Basanta-khatri-0311 <khatribasanta.git@gmail.com>
2024-08-09 22:55:03 +0545

    created a function that introduces me.

---
+++ Javascript/020_functions/script.js
@@ -0,0 +1,16 @@
+//The below done is known as function definition
+
+function intro(userName,post) {
+  console.log('Hi,')
+  console.log(`My name is ${userName}.`)
+  console.log(`I am a ${post}.`)
+
+  console.log('***********************************************')
+  return 'hello'
+}
+//this is called function calling
+// const value = intro()
+
+// intro('Basanta Khatri','Web Developer')
+// intro(`Something`,'Don`t know')
+

ec645d07170f24506bafdedec8f1bd0689d5ccba
Basanta-khatri-0311 <khatribasanta.git@gmail.com>
2024-08-09 15:58:01 +0545

    printing different patterns using for loop

--- Javascript/019_loopsin_js/whilescript.js
+++ Javascript/019_loopsin_js/whilescript.js
@@ -1,4 +1,7 @@
-console.log('Program Started');
+// console.log('Program Started');
+
+
+//----------------------------- while loops------------------------------//
 
 // let num = 0
 
@@ -15,14 +18,102 @@ console.log('Program Started');
 //   i++
 // }
 
-let friends = ['Ram', 'Shyam', 'Hari', 'Sita', 'Gita']
+// let friends = ['Ram', 'Shyam', 'Hari', 'Sita', 'Gita']
 
-let i = 0
+// let i = 0
 
-while (i < friends.length) {
-  console.log(`${i + 1}. ${friends[i]} `);
-  friends[i] = friends[i] + ' coder'
-  i++
-}
+// while (i < friends.length) {
+//   console.log(`${i + 1}. ${friends[i]} `);
+//   friends[i] = friends[i] + ' coder'
+//   i++
+// }
 
-console.log('Program Ended');
+// console.log('Program Ended');
+
+
+//------------------------------------------------For Loops----------------------------------------//
+
+console.log('Program started');
+
+//even numbers
+// for (let i = 0; i <= 100; i++) {
+//   if (i % 2 === 0) {
+//     console.log(i);
+//   }
+// }
+
+//odd numbers
+// for (let i = 0; i <= 100; i++) {
+//   if (i % 2 === 1) {
+//     console.log(i);
+//   }
+// }
+
+//for printing
+//
+//*
+//** 
+//***
+//**** 
+
+// for (let i = 1; i < 5; i++){
+//   let str = '*'
+
+//   for(let j = 1; j< i; j++){
+//     str+='*'
+//   }
+//   console.log(str);
+// }
+
+//for printing
+//***** 
+//**** 
+//*** 
+//**
+//*
+
+// for (let i = 1; i <= 5; i++) {
+//   let str = '*'
+//   for (j = 5; j > i; j--) {
+//     str += '*'
+//   }
+//   console.log(str);
+// }
+
+//for printing
+//   *
+//  ***
+// *****
+//******* 
+
+// for (let i = 1; i <= 4; i++) {
+//   let str = ''
+
+//   for (let j = 1; j <= 4-i; j++) {
+//     str += ' '
+//   }
+
+//   for (let k = 0; k < 2 * i - 1; k++) {
+//     str += '*'
+//   }
+//   console.log(str);
+// }
+
+
+//for printing
+// *******
+//  *****
+//   ***
+//    *
+
+
+// for (let i = 4; i >= 1; i--) {
+//   let str = ''
+//   for (let j = 0; j < 4 -i ; j++) {
+//     str += ' '
+//   }
+//   for (let k = 1; k <= i*2-1 ; k++) {
+//     str += '*'
+//   }
+//   console.log(str);
+// }

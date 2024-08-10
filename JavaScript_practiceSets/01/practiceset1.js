855ecce0cbff9ff09732bf24798f76c28aa18c18
Basanta-khatri-0311 <khatribasanta.git@gmail.com>
2024-08-10 12:20:43 +0545

    added the questions to it

--- JavaScript_practiceSets/01/practiceset1.js
+++ JavaScript_practiceSets/01/practiceset1.js
@@ -2,29 +2,47 @@
 // -----------------------------------Practice set-1------------------------------------------------//
 
 
-//creating variables and adding 2 different variable types
+//Q.1....creating variable OF TYPE STRING  and add a number to it.
+
+// --> Answer
+
 // let str1 = "Basanta"
 // let str2 = 12
 // console.log(str1 + str2);
 
-// //finding the type of 
+//Q.2....find the datatype of the variable and evaluated value in the above question
+
+// --> Answer
+
 // console.log(typeof (str1));
 // console.log(typeof (str2));
 // console.log(typeof (str1 + str2));
 
 
-//changing the value of a variable typed object to hold a string / number
+//Q.3....Create a const type object and check if you can change it to hold a number later
+
+// --> Answer
+
 // const a = {
 //   name: 'Basanta',
 //   class: 12
 // }
 // a = 'Khatri'
 // a =12
+
+//Q.4....Try adding a new key to the const object above.
+
+// --> Answer
+
 // a['friend'] ='Unknown'// can add a new value inside it
 // a.friend = 'unknown'// can add a new value inside it
 
 // console.log(a);
 
+
+//Q.5....Create a dictionary of 5 words
+
+// --> Answer
 
 // const wordMeaning = {
 //   something: ``,

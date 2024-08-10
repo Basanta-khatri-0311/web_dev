7e40e9a5f789246aa160f1b6b44b6136f058c8dc
Basanta-khatri-0311 <khatribasanta.git@gmail.com>
2024-08-10 14:48:37 +0545

    set 3 completed

---
+++ JavaScript_practiceSets/03/set3.js
@@ -0,0 +1,61 @@
+// ------------------------------------------------------Practice set 3--------------------------------------------------------------------------//
+
+
+//Q.1....Program to print marks of a student in an object using for loop.
+
+// --> Answer
+
+// const marks = {
+//   physics: 92,
+//   chemistry: 96,
+//   maths: 99,
+//   nepali: 88,
+//   english: 92,
+// }
+// for (let i = 0; i < Object.keys(marks).length; i++) {
+//   console.log(`The marks of ${Object.keys(marks)[i]} are ${marks[Object.keys(marks)[i]]}`);
+// }
+
+
+//Q.2....program same as q.1 using for in loop
+
+// --> Answer
+
+// for(let i in marks){
+//   console.log(`The marks in ${i} is ${marks[i]}`);
+// }
+
+
+
+//Q.3....program to print try again until the correct number is entered
+
+// --> Answer
+
+
+// let correctNumber = 12
+// let num
+
+// while (num != correctNumber) {
+//   num = prompt('Enter a matching number: ')
+//   console.log('Try Agian..!!!');
+// }
+// console.log('You entered a correct number');
+
+
+
+
+
+//Q.4....Function to find mean of 5 numbers
+
+// --> Answer
+
+// function mean(a, b, c, d) {
+//   return (a + b + c + d) / 4
+// }
+
+// console.log(mean(2,4,5,6));
+
+
+
+
+

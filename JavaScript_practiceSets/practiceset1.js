885dce96d7323953c8d0ad3c805119d155ba2d5a
Basanta-khatri-0311 <khatribasanta.git@gmail.com>
2024-08-10 12:07:16 +0545

    practice set 1 fro variables and objects

---
+++ JavaScript_practiceSets/practiceset1.js
@@ -0,0 +1,37 @@
+
+// -----------------------------------Practice set-1------------------------------------------------//
+
+
+//creating variables and adding 2 different variable types
+// let str1 = "Basanta"
+// let str2 = 12
+// console.log(str1 + str2);
+
+// //finding the type of 
+// console.log(typeof (str1));
+// console.log(typeof (str2));
+// console.log(typeof (str1 + str2));
+
+
+//changing the value of a variable typed object to hold a string / number
+// const a = {
+//   name: 'Basanta',
+//   class: 12
+// }
+// a = 'Khatri'
+// a =12
+// a['friend'] ='Unknown'// can add a new value inside it
+// a.friend = 'unknown'// can add a new value inside it
+
+// console.log(a);
+
+
+// const wordMeaning = {
+//   something: ``,
+//   somewhere: ``,
+//   unknown: ``,
+//   people: ``,
+//   planet: ``
+// }
+
+// console.log(wordMeaning);

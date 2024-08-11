785c88d0356da9f62e3518ea3d0392c1f6258b09
Basanta-khatri-0311 <khatribasanta.git@gmail.com>
2024-08-11 12:23:23 +0545

    scopes in js

---
+++ Javascript/023_scope_in_js/script.js
@@ -0,0 +1,36 @@
+// const userName = 'Basanta Khatri'
+// const userAge = 22
+
+// function add() {
+//   const userName ='Someone'
+//   const x = 5
+//   const y = 8
+//   console.log(x + y);
+//   console.log(userName);
+// }
+
+// function sub() {
+//   const x = 15
+//   const y = 18
+//   console.log(x - y);
+//   console.log(userName);
+
+//   function child(){
+//     const childName = 'Goli'
+//     console.log(childName);
+//   }
+//   child()
+// }
+
+
+
+// add()
+// sub()
+// console.log('Program Ended');
+
+
+function truy() {
+  c = `hello`
+  console.log('hi');
+}
+console.log(truy());

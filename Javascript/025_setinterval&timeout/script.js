c5618d917edc0c5133f58af5abf5afb88329de42
Basanta-khatri-0311 <khatribasanta.git@gmail.com>
2024-08-12 21:30:57 +0545

    asyn and sync

--- Javascript/025_setinterval&timeout/script.js
+++ Javascript/025_setinterval&timeout/script.js
@@ -1,7 +1,21 @@
 // const timer1 = setTimeout(`console.log("hello-1")`, 1000)
 // const timer2 = setTimeout(`console.log("hello-2")`, 2000)
-// const timer3 = setTimeout(`console.log("hello-3")`, 3000)
+// const timer3 = setTimeout(a, 3000)//H.O.F
+// const timer1 = setInterval(`console.log("hello-1")`, 1000)
+// const timer2 = setTimeout(`console.log("hello-2")`, 2000)
+// const timer3 = setInterval(a, 3000)//    H.O.F
+
+setTimeout(function (){
+  console.log('hii')   //asynchronous
+},2000)
 
 // clearTimeout(timer1)
 // clearTimeout(timer2)
 // clearTimeout(timer3)
+
+function a(){
+  console.log(`Hello World..!`);
+}
+
+console.log(`Hello Worldddd..!`); //synchronous code
+

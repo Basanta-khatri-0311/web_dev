9730417b04552d95c3ea897d2d0e8af02af543f6
Basanta-khatri-0311 <khatribasanta.git@gmail.com>
2024-08-12 13:37:56 +0545

    learning about higher order functions and callback with closure

---
+++ Javascript/024_higher_order_function/script.js
@@ -0,0 +1,24 @@
+// function a(b){
+//   // console.log(typeof(b));
+//   console.dir(b);
+
+//   b()
+// }
+
+// // a('hi')
+// // a({userName: 'Basanta Khatri', age: 22})
+// // a([1,2,3,4,5,6,7])
+
+// function sayHi(){
+//   console.log('hiiiiiii');
+// }
+
+
+// // call back function
+// a(function(){
+//   console.log('hiiiiiii');
+// })
+
+
+
+

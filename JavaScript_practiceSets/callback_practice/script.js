c56c5af61a87340f9a7232cd900ca795068131c1
Basanta-khatri-0311 <khatribasanta.git@gmail.com>
2024-08-12 14:20:35 +0545

    some more questions

--- JavaScript_practiceSets/callback_practice/script.js
+++ JavaScript_practiceSets/callback_practice/script.js
@@ -33,3 +33,43 @@ function sqr(num) {
 const num = [1, 2, 3, 4, 5]
 
 processArray(num, sqr)
+
+//Filtering an Array
+//Write a function filterArray that takes an array and a callback function. 
+//The function should return a new array containing only the elements for which the 
+//callback function returns true.
+
+function filterArray(arr, callBack) {
+  const even = []
+  for (let i = 0; i < arr.length; i++){
+    if(callBack(arr[i])){
+      even.push(arr[i])
+    }
+  }
+  console.log(even);
+}
+
+function isTrue(num) {
+    return num % 2===0
+}
+
+filterArray([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], isTrue)
+
+
+//Mapping an Array
+//Create a function mapArray that takes an array and a callback function. 
+//The function should return a new array with each element transformed by the callback function.
+
+function mapArray(arr,callBack){
+  const upperValue = []
+  for(let i =0;i<arr.length ;i++){
+    upperValue.push(callBack(arr[i]))
+  }
+  console.log(upperValue);
+}
+
+function toUpper(value){
+  return value.toUpperCase()
+}
+
+mapArray(['apple','mango','banana','grapes','watermelon'],toUpper)

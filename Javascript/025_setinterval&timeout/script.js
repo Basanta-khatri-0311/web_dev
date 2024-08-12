f6d69173aec5b38228f7bb510e265287bf207220
Basanta-khatri-0311 <khatribasanta.git@gmail.com>
2024-08-12 17:51:03 +0545

    set timeout as normal

---
+++ Javascript/025_setinterval&timeout/script.js
@@ -0,0 +1,7 @@
+// const timer1 = setTimeout(`console.log("hello-1")`, 1000)
+// const timer2 = setTimeout(`console.log("hello-2")`, 2000)
+// const timer3 = setTimeout(`console.log("hello-3")`, 3000)
+
+// clearTimeout(timer1)
+// clearTimeout(timer2)
+// clearTimeout(timer3)

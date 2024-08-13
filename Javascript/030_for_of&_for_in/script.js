4100cb2b19ff0a39da7fb99ac54fd1b59e3efbaf
Basanta-khatri-0311 <khatribasanta.git@gmail.com>
2024-08-13 16:40:14 +0545

    for each loop

--- Javascript/030_for_of&_for_in/script.js
+++ Javascript/030_for_of&_for_in/script.js
@@ -1,42 +1,42 @@
-const fruit = ['apple', 'banana', 'grapes', 'water-melon']
+// const fruit = ['apple', 'banana', 'grapes', 'water-melon']
 
-for (let i = 0; i < fruit.length; i++) {
-  console.log(fruit[i] = i);
-}
-console.log('--------------------------------------------------------------')
+// for (let i = 0; i < fruit.length; i++) {
+//   console.log(fruit[i] = i);
+// }
+// console.log('--------------------------------------------------------------')
 
-for (value of fruit) {
-  console.log(value);
-}
-console.log('--------------------------------------------------------------')
-const userNames = 'Basanta Khatri'
+// for (value of fruit) {
+//   console.log(value);
+// }
+// console.log('--------------------------------------------------------------')
+// const userNames = 'Basanta Khatri'
 
-for (const userName of userNames) {
-  if (userName === ' ') {
-    console.log('This is a space');
-  } else {
-    console.log(userName);
-  }
-}
+// for (const userName of userNames) {
+//   if (userName === ' ') {
+//     console.log('This is a space');
+//   } else {
+//     console.log(userName);
+//   }
+// }
 
-console.log('--------------------------------------------------------------')
+// console.log('--------------------------------------------------------------')
 
-const person = {
-  firstName: 'John',
-  lastName: 'Doe',
-  age: 30,
-  city: 'Banglore'
-}
+// const person = {
+//   firstName: 'John',
+//   lastName: 'Doe',
+//   age: 30,
+//   city: 'Banglore'
+// }
 
-for (const key in person) {
-  console.log(person[key]);
-}
-console.log('--------------------------------------------------------------')
-const personKey = Object.keys(person)
+// for (const key in person) {
+//   console.log(person[key]);
+// }
+// console.log('--------------------------------------------------------------')
+// const personKey = Object.keys(person)
 
-for (const key of personKey) {
-  console.log(person[key]);
-}
+// for (const key of personKey) {
+//   console.log(person[key]);
+// }
 //const personKey = Object.keys(person)
 //const personValue = Object.values(person)
 //const personEntries = Object.entries(person)
@@ -46,3 +46,11 @@ for (const key of personKey) {
 // const string1 ='Basanta Khatri'
 
 // console.log( string1.split('').reverse().join(''))
+
+
+//FOR EACH LOOP
+const fruits = ['apple', 'banana', 'grapes', 'water-melon']
+
+fruits.forEach((fruit)=>{
+  console.log(fruit);
+})

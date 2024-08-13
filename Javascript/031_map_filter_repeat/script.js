441a8ec1de6db6602f0c50abd423aee2f4d8bf65
Basanta-khatri-0311 <khatribasanta.git@gmail.com>
2024-08-13 21:05:41 +0545

    map,filter,reduce

---
+++ Javascript/031_map_filter_repeat/script.js
@@ -0,0 +1,55 @@
+const months = ['January', 'February', 'March', 'April', 'May',
+  'June', 'July', 'August', 'September', 'October', 'November',
+  'December']
+
+// const obMonths = {
+//   january: 1,
+//   february: 2,
+//   march: 3,
+//   april: 4,
+//   may: 5
+// }
+
+// months.forEach((month)=>{
+//   console.log(month);
+// })
+
+// const capMonths = months.forEach((month,index)=>{
+//   console.log(index+1,month);
+//   return month.toLocaleUpperCase()
+// })
+
+// const capMonths = months.map((month,index,array)=>{
+//   console.log(index+1,month);
+//   // console.log(array);
+//   return month.toLocaleUpperCase()
+// })
+
+
+//Returns months having m or M.
+// const filterMonths = months.filter((month, index) => {
+//   console.log(month);
+//   return month.includes('m') || month.includes('M')
+// })
+
+//returns months in odd place
+const filterMonths = months.filter((month, index) => {
+  console.log(month);
+  return (index + 1) % 2 === 1
+})
+
+
+
+
+// const key = Object.keys(obMonths)
+// console.log(key);
+
+// for(keys in key){
+//   console.log(obMonths[key[keys]]);
+// }
+
+
+// for(keys of key){
+//   console.log(obMonths[keys]);
+// }
+

5cb947cfe4eb712510692dd6b6d706b1997a984e
Basanta-khatri-0311 <khatribasanta.git@gmail.com>
2024-08-16 22:39:54 +0545

    selecting the certain element or attribute in js

--- HTML/02_roadmap_frontend/script.js
+++ HTML/02_roadmap_frontend/script.js
@@ -1,7 +1,14 @@
 
 
-// const imagesCollection = document.getElementsByTagName('img')
 // const imagesCollection1 = document.images
+// const imagesCollection = document.getElementsByTagName('img')
 
-const cssImage = document.getElementsByClassName('css-image')
+// const cssImage = document.getElementsByClassName('css-image')
 
+// const realImage =document.getElementById()
+
+// document.querySelector('#css-image') 
+// document.querySelectorAll('#css-image') //gives node list
+
+
+// const jsImage = document.querySelector('.js-image')

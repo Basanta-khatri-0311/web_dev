f7710396669bf9bfadae5f1fd1dc991f43a2f543
Basanta-khatri-0311 <khatribasanta.git@gmail.com>
2024-08-20 23:59:06 +0545

    different key events

--- Javascript/038_form_events/keyboardscript.js
+++ Javascript/038_form_events/keyboardscript.js
@@ -1,4 +1,8 @@
 // const hello = document.querySelector('h1')
 
+window.addEventListener('key',(e)=>console.log(e.key))
+window.addEventListener('key',(e)=>console.log(e.code))
 window.addEventListener('keyup',(e)=>console.log(e.key))
 window.addEventListener('keyup',(e)=>console.log(e.code))
+window.addEventListener('keydown',(e)=>console.log(e.key))
+window.addEventListener('keydown',(e)=>console.log(e.code))

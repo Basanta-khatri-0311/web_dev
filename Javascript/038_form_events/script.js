c71dfca2c02f390ede0595d04694753c838d1df3
Basanta-khatri-0311 <khatribasanta.git@gmail.com>
2024-08-20 22:43:10 +0545

    form events in js

--- Javascript/038_form_events/script.js
+++ Javascript/038_form_events/script.js
@@ -1,11 +1,14 @@
 const userNameInput = document.querySelector('#username')
 const para = document.querySelector('p')
+const form = document.querySelector('form')
 
 userNameInput.addEventListener('click', () => {
   console.log('input field clicked')
 })
 
+form.addEventListener('click',(e)=>console.log(e))
+
 // userNameInput.addEventListener('input', (e) => para.innerText = e.target.value)  input event
 // userNameInput.addEventListener('change', (e) => para.innerText = e.target.value)  change event
 // userNameInput.addEventListener('focus', (e) => para.innerText = e.target.value) // focus event
-userNameInput.addEventListener('blur', (e) => para.innerText = e.target.value) // blur event
+// userNameInput.addEventListener('blur', (e) => para.innerText = e.target.value) // blur event

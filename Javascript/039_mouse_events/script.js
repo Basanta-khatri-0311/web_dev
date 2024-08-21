0e7abae20db6d11b3e5b7195ee3b11f024e1b77e
Basanta-khatri-0311 <khatribasanta.git@gmail.com>
2024-08-21 23:06:55 +0545

    different mouse events

--- Javascript/039_mouse_events/script.js
+++ Javascript/039_mouse_events/script.js
@@ -4,7 +4,49 @@ const container = document.querySelector('.container')
 
 let count = 0
 
-card.addEventListener('click', () => {
+//runs while clicking
+// card.addEventListener('click', () => {
+//   const newCard = document.createElement('div')
+//   newCard.classList.add('card')
+//   newCard.innerText = count++
+//   container.append(newCard)
+// })
+
+
+//runs immediately after clicking
+// card.addEventListener('mousedown', () => {
+//   const newCard = document.createElement('div')
+//   newCard.classList.add('card')
+//   newCard.innerText = count++
+//   container.append(newCard)
+// })
+
+//runs after the mouse is lifted after clicked
+// card.addEventListener('mouseup', () => {
+//   const newCard = document.createElement('div')
+//   newCard.classList.add('card')
+//   newCard.innerText = count++
+//   container.append(newCard)
+// })
+
+//runs after the mouse enters the area
+// card.addEventListener('mouseenter', () => {
+//   const newCard = document.createElement('div')
+//   newCard.classList.add('card')
+//   newCard.innerText = count++
+//   container.append(newCard)
+// })
+
+//runs after the mouse leaves the area
+card.addEventListener('mouseleave', () => {
+  const newCard = document.createElement('div')
+  newCard.classList.add('card')
+  newCard.innerText = count++
+  container.append(newCard)
+})
+
+//runs as much the mouse moves in that area
+card.addEventListener('mousemove', () => {
   const newCard = document.createElement('div')
   newCard.classList.add('card')
   newCard.innerText = count++

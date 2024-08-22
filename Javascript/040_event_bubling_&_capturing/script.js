be46df5eade18091e113fc9926dd5c8f43c0760a
Basanta-khatri-0311 <khatribasanta.git@gmail.com>
2024-08-22 23:34:20 +0545

    captures the click using capture true same as use capture

--- Javascript/040_event_bubling_&_capturing/script.js
+++ Javascript/040_event_bubling_&_capturing/script.js
@@ -2,13 +2,23 @@ const green = document.querySelector('.green')
 const pink = document.querySelector('.pink')
 const blue = document.querySelector('.blue')
 
+// green.addEventListener('click',(e)=>{
+//   console.log('Green event listener');
+// },true)
+// pink.addEventListener('click',(e)=>{
+//   console.log('Pink event listener');
+// },true)
+// blue.addEventListener('click',(e)=>{
+//   e.stopPropagation()
+//   console.log('Blue event listener');
+// },true)
 green.addEventListener('click',(e)=>{
   console.log('Green event listener');
-},true)
+},{capture:true})
 pink.addEventListener('click',(e)=>{
   console.log('Pink event listener');
-},true)
+},{capture:true})
 blue.addEventListener('click',(e)=>{
   e.stopPropagation()
   console.log('Blue event listener');
-},true)
+},{capture:true})

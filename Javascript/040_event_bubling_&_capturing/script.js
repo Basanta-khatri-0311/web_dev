3eac619d8d86f951dfe15ed2c5ae456cc5f6e55d
Basanta-khatri-0311 <khatribasanta.git@gmail.com>
2024-08-22 23:27:21 +0545

    element bubbling, clicking child will bubble the parent event as wel

--- Javascript/040_event_bubling_&_capturing/script.js
+++ Javascript/040_event_bubling_&_capturing/script.js
@@ -1,3 +1,13 @@
 const green = document.querySelector('.green')
 const pink = document.querySelector('.pink')
 const blue = document.querySelector('.blue')
+
+green.addEventListener('click',(e)=>{
+  console.log('Green event listener');
+})
+pink.addEventListener('click',(e)=>{
+  console.log('Pink event listener');
+})
+blue.addEventListener('click',(e)=>{
+  console.log('Blue event listener');
+})

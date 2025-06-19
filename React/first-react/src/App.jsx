aa28071c2d59b40f097450ede2ec7f9cfa79bed8
Basanta-khatri-0311 <khatribasanta.git@gmail.com>
2025-06-19 13:26:46 +0545

    creating project with npm

---
+++ React/first-react/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <img src="./src/assets/reactlogo.jpg" alt="" />
+      <h1>Anything You want!!!</h1>
+    </div>
+  )
+}
+
+export default App

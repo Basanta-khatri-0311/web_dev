8a831016ea0ca23bb5aa4ec151699475b6344c53
Basanta-khatri-0311 <khatribasanta.git@gmail.com>
2025-06-22 10:14:21 +0545

    adding product to a cart

--- gfgreact/src/App.jsx
+++ gfgreact/src/App.jsx
@@ -1,3 +1,38 @@
+// import React, { useState } from "react";
+// const allBrands = [
+//   { id: "1", brandName: "adiddas" },
+//   { id: "2", brandName: "nike" },
+//   { id: "3", brandName: "adapt" },
+//   { id: "4", brandName: "huba" },
+//   { id: "5", brandName: "puma" },
+// ];
+// function App() {
+//   const [search, setSearch] = useState("");
+//   const [brands, setBrands] = useState(allBrands);
+
+//   const onSearchChange = (e) => {
+//     const input = e.target.value;
+//     setSearch(input);
+//     const filteredBrand = allBrands.filter((brand) =>
+//       brand.brandName.toLowerCase().includes(input.toLowerCase())
+//     );
+//     setBrands(filteredBrand);
+//   };
+
+//   return (
+//     <div>
+//       <input type="text" name="" id="" onChange={onSearchChange} value={search}/>
+//       <ul>
+//         {brands.map((value) => (
+//           <li key={value.id}>{value.brandName}</li>
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// }
+
+// export default App;
+
 import React, { useState } from "react";
 const allBrands = [
   { id: "1", brandName: "adiddas" },
@@ -6,28 +41,47 @@ const allBrands = [
   { id: "4", brandName: "huba" },
   { id: "5", brandName: "puma" },
 ];
-function App() {
-  const [search, setSearch] = useState("");
-  const [brands, setBrands] = useState(allBrands);
 
-  const onSearchChange = (e) => {
-    const input = e.target.value;
-    setSearch(input);
-    const filteredBrand = allBrands.filter((brand) =>
-      brand.brandName.toLowerCase().includes(input.toLowerCase())
-    );
-    setBrands(filteredBrand);
+function App() {
+  const [selectedBrand, setSelectedBrand] = useState([]);
+
+  const onAddToCartClick = (clickedBrand) => {
+    const foundBrand = allBrands.find((value) => value.id === clickedBrand);
+    if (!selectedBrand.some((b) => b.id === clickedBrand)) {
+      setSelectedBrand((prev) => [...prev, foundBrand]);
+    }else{
+      alert("The item is already in the cart")
+    }
   };
 
   return (
-    <div>
-      <input type="text" name="" id="" onChange={onSearchChange} value={search}/>
-      <ul>
-        {brands.map((value) => (
-          <li key={value.id}>{value.brandName}</li>
+    <>
+      <div>
+        <h1>Our Brands</h1>
+        {allBrands.map((brand) => (
+          <div key={brand.id}>
+            <span>{brand.brandName}</span>
+            <button onClick={() => onAddToCartClick(brand.id)}>
+              Add To Cart
+            </button>
+          </div>
         ))}
-      </ul>
-    </div>
+      </div>
+      <div>
+        {selectedBrand.length > 0 ? (
+          <div>
+            <h2>Cart Products</h2>
+            <ul>
+              {selectedBrand.map((brand) => (
+                <li key={brand.id}>{brand.brandName}</li>
+              ))}
+            </ul>
+          </div>
+        ) : (
+          <h2>No Products In Cart</h2>
+        )}
+      </div>
+    </>
   );
 }
 

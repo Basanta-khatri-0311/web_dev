dbcc6bbb1cb233eae1fc4b3996f2d6deaf1967de
Basanta-khatri-0311 <khatribasanta.git@gmail.com>
2025-07-01 13:37:33 +0545

    usereducer for cart functionality

--- gfgreact/src/components/ReducerRevision.jsx
+++ gfgreact/src/components/ReducerRevision.jsx
@@ -14,15 +14,35 @@ const ReducerRevision = () => {
 
         if (exists) {
           // If already in cart, increase quantity
-          return state.map((cartItem) =>
-            cartItem.id === item.id
-              ? { ...cartItem, quantity: cartItem.quantity + 1 }
-              : cartItem
-          );
+          alert("Iteam is alread in the cart");
+          return state;
         } else {
           // Else add with quantity 1
           return [...state, { ...item, quantity: 1 }];
         }
+      case "INCREASEQUANTITY":
+        const increasingItem = action.payload;
+        return state.map((cartItem) =>
+          cartItem.id === increasingItem.id
+            ? { ...cartItem, quantity: cartItem.quantity + 1 }
+            : cartItem
+        );
+      case "DECREASEQUANTITY":
+        const decreasingItem = action.payload;
+        return state.map((cartItem) =>
+          cartItem.id === decreasingItem.id
+            ? {
+                ...cartItem,
+                quantity: cartItem.quantity > 1 ? cartItem.quantity - 1 : 1,
+              }
+            : cartItem
+        );
+      case "REMOVEPRODUCTFROMCART":
+        const removingProduct = action.payload;
+        return state.filter((cartItem) => cartItem.id !== removingProduct.id);
+
+      case "CLEARCART" :
+        return []
       default:
         return state;
     }
@@ -32,6 +52,26 @@ const ReducerRevision = () => {
     const selectedProduct = products.find((p) => p.id === id);
     cartDispatch({ type: "ADDTOCART", payload: selectedProduct });
   };
+
+  const handelQuantityIncrease = (id) => {
+    const increasedProduct = products.find((p) => p.id === id);
+    cartDispatch({ type: "INCREASEQUANTITY", payload: increasedProduct });
+  };
+
+  const handelQuantityDecrease = (id) => {
+    const decreasedProduct = products.find((p) => p.id === id);
+    cartDispatch({ type: "DECREASEQUANTITY", payload: decreasedProduct });
+  };
+
+  const handelRemoveFromCart = (id) => {
+    const removingProduct = products.find((p) => p.id === id);
+    cartDispatch({ type: "REMOVEPRODUCTFROMCART", payload: removingProduct });
+  };
+
+  const handelClearCart = () => {
+    cartDispatch({type: "CLEARCART"})
+  }
+
   const [cartProducts, cartDispatch] = useReducer(
     cartDispacher,
     initialCartValues
@@ -53,10 +93,23 @@ const ReducerRevision = () => {
         </div>
       </div>
       <h2>Cart Items</h2>
+      <button onClick={handelClearCart}>Clear Cart</button>
       <ul>
         {cartProducts.map((item) => (
           <li key={item.id}>
             {item.name} x {item.quantity} = NPR {item.price * item.quantity}
+            <button onClick={() => handelQuantityIncrease(item.id)}>
+              Increase
+            </button>
+            <button
+              onClick={() => handelQuantityDecrease(item.id)}
+              disabled={item.quantity === 1}
+            >
+              Decrease
+            </button>
+            <button onClick={() => handelRemoveFromCart(item.id)}>
+              Remove From Cart
+            </button>
           </li>
         ))}
       </ul>
